8ae6adb2c329495d00395d239721ddd5d2da187a
fiahfy <d.fiahfy@gmail.com>
2018-08-30 08:31:53 +0900

    Fix header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,11 +2,10 @@
   <v-app>
     <v-toolbar app>
       <v-spacer />
-      <img
-        src="~/assets/icon.svg"
-        class="mb-2"
-      >
-      <span class="headline">addy</span>
+      <h1>
+        <img src="~/assets/icon.svg">
+        <span class="headline">addy</span>
+      </h1>
       <v-spacer />
     </v-toolbar>
     <v-content>
@@ -14,16 +13,6 @@
     </v-content>
   </v-app>
 </template>
-
-<script>
-import AppIcon from '~/components/AppIcon'
-
-export default {
-  components: {
-    AppIcon
-  }
-}
-</script>
 
 <style>
 html, body, #__nuxt, #__layout, #app, main, .v-content__wrap>div, .swiper-container {
